998dc454c0b98a8e9971566051ca12eb0b76134d
Alexis <aadolpho@ucsc.edu>
2022-04-24 01:27:40 -0700

    changed background

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-let config = {
-    type: Phaser.CANVAS,
+var config = {
+    type: Phaser.AUTO,
     width: 640,
     height: 480,
     scene: [ Menu, Play ]
 }
+  
+  var game = new Phaser.Game(config);
+  
+  function preload() {
+    this.load.image('yayback', 'yayback.png');
+  }
+  
+  
+  function create() {
+    this.add.image(640, 480, 'yayback');
+  }
+  
+  function update() {
+  
+  }
 
-let game = new Phaser.Game(config);
+//let config = {
+//    type: Phaser.CANVAS,
+//    width: 640,
+//    height: 480,
+//    scene: [ Menu, Play ]
+//}
+
+//let game = new Phaser.Game(config);
 
 // set UI sizes
 let borderUISize = game.config.height / 15;
@@ -13,3 +35,12 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+
+
+    //scene: {
+      //  preload: preload,
+        //create: create,
+        //update: update
+    //}
+  //};
