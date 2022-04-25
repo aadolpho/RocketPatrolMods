d3ba6e8bad79340f49ddc6e48964b17da63db42f
Alexis <aadolpho@ucsc.edu>
2022-04-25 11:40:09 -0700

    Changed colors for title screen

--- src/main.js
+++ src/main.js
@@ -1,33 +1,11 @@
 var config = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     width: 640,
     height: 480,
     scene: [ Menu, Play ]
 }
   
-  var game = new Phaser.Game(config);
-  
-  function preload() {
-    this.load.image('yayback', 'yayback.png');
-  }
-  
-  
-  function create() {
-    this.add.image(640, 480, 'yayback');
-  }
-  
-  function update() {
-  
-  }
-
-//let config = {
-//    type: Phaser.CANVAS,
-//    width: 640,
-//    height: 480,
-//    scene: [ Menu, Play ]
-//}
-
-//let game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
 // set UI sizes
 let borderUISize = game.config.height / 15;
@@ -36,11 +14,3 @@ let borderPadding = borderUISize / 3;
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT;
 
-
-
-    //scene: {
-      //  preload: preload,
-        //create: create,
-        //update: update
-    //}
-  //};
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,7 +15,8 @@ class Menu extends Phaser.Scene {
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
+            //backgroundColor: '#F3B141',
+            backgroundColor: '#FAEBD7',
             color: '#843605',
             align: 'right',
             padding: {
@@ -28,7 +29,7 @@ class Menu extends Phaser.Scene {
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.backgroundColor = '#FAEBD7';
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
